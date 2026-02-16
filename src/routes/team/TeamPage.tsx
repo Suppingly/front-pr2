3d3637c34cdc274f88c8ceac738c22bf90434934
Suppingly <kul1kov.alexandr@mail.ru>
2026-02-16 11:29:47 +0300

    Add files via upload

--- src/routes/team/TeamPage.tsx
+++ src/routes/team/TeamPage.tsx
@@ -10,8 +10,43 @@ import dotImg from '../../assets/Ellipse 19.png'
 import arrowImg2 from '../../assets/arrow (2).png'
 import arrowImg3 from '../../assets/arrow (3).png'
 import './Team.css'
+import { useState } from 'react'
+
+interface teamMember{
+  img:String;
+  name: String;
+  job: String;
+  hasLinks: Boolean;
+}
+interface section{
+  img:String;
+  alt:String;
+}
 
 function TeamPage(){
+  const [teamMembers,setMembers]=useState<teamMember[]>([
+    {
+      img:humanImg,
+      name:'Aimee Calderon',
+      job:'Веб-разработчик',
+      hasLinks:false
+    },
+    {
+      img:humanImg1,
+      name:'Raymond Horn',
+      job:'Цифровой маркетолог',
+      hasLinks:true
+    },
+    {
+      img:humanImg2,
+      name:'Linda Towner',
+      job:'Дизайнер приложений',
+      hasLinks:false
+    }
+  ])
+  const [sections,setSections]=useState<section[]>([
+    { img:dotAcImg,alt:'-' },{ img:dotImg,alt:'0' },{ img:dotImg,alt:'0' }
+  ])
   return (
     <>
       <div className="teamContainer">
@@ -27,50 +62,70 @@ function TeamPage(){
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
             </p>
             <div className="buttons">
-              <button className="button">
+              <button className="button" onClick={()=>{
+                let newList=[]
+                let newSectionList=[]
+                newList.push(teamMembers[teamMembers.length-1])
+                for (let i=0;i<teamMembers.length;i++){
+                  if (i!=teamMembers.length-1)
+                    newList[i+1]=teamMembers[i]
+                }
+                setMembers(newList)
+                for (let i=0;i<sections.length;i++){
+                  if (i!=0)
+                    newSectionList[i-1]=sections[i]
+                }
+                newSectionList.push(sections[0])
+                setSections(newSectionList)
+              }}>
                 <img src={arrowImg3} alt="<-" />
               </button>
-              <button className="button">
+              <button className="button" onClick={()=>{
+                let newList=[]
+                let newSectionList=[]
+                newList.push(teamMembers[teamMembers.length-1])
+                for (let i=0;i<teamMembers.length;i++){
+                  if (i!=0){
+                    newList[i-1]=teamMembers[i]
+                  }
+                }
+                newList.push(teamMembers[0])
+                setMembers(newList)
+                newSectionList.push(sections[sections.length-1])
+                for (let i=0;i<sections.length;i++){
+                  if (i!=sections.length-1)
+                    newSectionList[i+1]=sections[i]
+                }
+                setSections(newSectionList)
+              }}>
                 <img src={arrowImg2} alt="->" />
               </button>
             </div>
           </div>
         </div>
         <div className="teamGrid">
-          <div>
-            <img src={humanImg} alt="Aimee Calderon" className="teamImg"/>
-            <div className="teamInfo">
-              <h3 className="teamName">Aimee Calderon</h3>
-              <p className="teamJob">Веб-разработчик</p>
-            </div>
-          </div>
-          <div>
+          {teamMembers.map((memberCard)=>(
+          <div className='teamCard'>
             <div className='relative'>
-              <img src={humanImg1} alt="Raymond Horn" className="teamImg"/>
-              <div className="teamSoc">
-                <a href=""><img src={socialImg} alt='f'/></a>
-                <a href=""><img src={socialImg1} alt='t'/></a>
-                <a href=""><img src={socialImg2} alt='i'/></a>
-                <a href=""><img src={socialImg3} alt='in'/></a>
-              </div>
+              <img src={memberCard.img} alt={memberCard.name} className="teamImg"/>
+              {memberCard.hasLinks && <div className="teamSoc">
+                <a href="" className='teamLink'><img src={socialImg} alt='f'/></a>
+                <a href="" className='teamLink'><img src={socialImg1} alt='t'/></a>
+                <a href="" className='teamLink'><img src={socialImg2} alt='i'/></a>
+                <a href="" className='teamLink'><img src={socialImg3} alt='in'/></a>
+              </div>}
             </div>
             <div className="teamInfo">
-              <h3 className="teamName">Raymond Horn</h3>
-              <p className="teamJob">Цифровой маркетолог</p>
+              <h3 className="teamName">{memberCard.name}</h3>
+              <p className="teamJob">{memberCard.job}</p>
             </div>
           </div>
-          <div>
-            <img src={humanImg2} alt="Linda Towner" className="teamImg"/>
-            <div className="teamInfo">
-              <h3 className="teamName">Linda Towner</h3>
-              <p className="teamJob">Дизайнер приложений</p>
-            </div>
-          </div>
+          ))}
         </div>
         <div className="teamPages">
-          <img src={dotAcImg} alt='-'/>
-          <img src={dotImg} alt='0'/>
-          <img src={dotImg} alt='0'/>
+          {sections.map((section)=>(
+            <img src={section.img} alt={section.alt}/>
+          ))}
         </div>
       </div>
     </>
